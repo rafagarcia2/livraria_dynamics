4aeaa861fa41fad2e511fad950ec8d05efa822fb
Rafael Garcia <rafaelgarcia2013@gmail.com>
2018-08-11 00:52:52 -0300

    Adicionando acesso ao Banco de Dados por meio do JPA

--- src/br/com/livraria/bean/LivroBean.java
+++ src/br/com/livraria/bean/LivroBean.java
@@ -2,17 +2,28 @@ package br.com.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 
+import br.com.livraria.dao.DAO;
+import br.com.livraria.modelo.Livro;
+
 @ManagedBean
 public class LivroBean {
 
-	private Livro livro;
+	private Livro livro = new Livro();
 
 	public Livro getLivro() {
 		return livro;
 	}
 
 	public void gravar() {
-		System.out.println("Gravando livro!" + this.livro.getTitulo());
+		System.out.println("Gravando livro " + this.livro.getTitulo());
+
+		if (livro.getAutores().isEmpty()) {
+			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+		}
+
+		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
 	}
 
 }
