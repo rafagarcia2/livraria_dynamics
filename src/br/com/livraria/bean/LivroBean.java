42b9ca18bf5dd1e85a7f6494b2ec0c1d5532f005
Rafael Garcia <rafaelgarcia2013@gmail.com>
2018-08-12 00:47:12 -0300

    Adicionando Vários Autores

--- src/br/com/livraria/bean/LivroBean.java
+++ src/br/com/livraria/bean/LivroBean.java
@@ -1,14 +1,24 @@
 package br.com.livraria.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.livraria.dao.DAO;
+import br.com.livraria.modelo.Autor;
 import br.com.livraria.modelo.Livro;
 
 @ManagedBean
-public class LivroBean {
+@ViewScoped
+public class LivroBean implements Serializable {
+
+	private static final long serialVersionUID = -484534310830408874L;
 
 	private Livro livro = new Livro();
+	
+	private Integer autorId;
 
 	public Livro getLivro() {
 		return livro;
@@ -24,6 +34,31 @@ public class LivroBean {
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
 		
 		this.livro = new Livro();
+	}
+	
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public List<Livro> getLivros() {
+	  return new DAO<Livro>(Livro.class).listaTodos();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+	}
+	
+	public List<Autor> getAutoresDoLivro() {
+		return this.livro.getAutores();
+	}
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
 	}
 
 }
